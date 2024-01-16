47f3bc4d2ce2bc0559e5e70b2dd21d7dbb8c0caa
KevinChristian0409 <christiankevin0409@gmail.com>
2024-01-15 20:41:46 -0500

    Upated file for server to  print env variables

--- src/logger.js
+++ src/logger.js
@@ -3,6 +3,12 @@ const options = { level: process.env.LOG_LEVEL || 'info' };
 
 // If we're doing `debug` logging, make the logs easier to read
 if (options.level === 'debug') {
+  const pino = require('pino');
+  const pretty = require('pino-pretty');
+  const logger = pino({ level: 'debug' }, pretty());
+
+  logger.debug({ environmentalVariables: process.env });
+
   // https://github.com/pinojs/pino-pretty
   options.transport = {
     target: 'pino-pretty',
