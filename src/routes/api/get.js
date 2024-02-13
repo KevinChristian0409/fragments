559b61ddaba0f25b19e67b47480f42fa9f29ddd5
KevinChristian0409 <christiankevin0409@gmail.com>
2024-02-12 19:52:16 -0500

    added routes and tests

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,17 +1,23 @@
 // src/routes/api/get.js
 
-const { createSuccessResponse } = require('../../../src/response');
+const { Fragment } = require('../../model/fragment');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
-  const responseData = {
-    fragments: [],
-  };
-
-  // Use createSuccessResponse for the response
-  const successResponse = createSuccessResponse(responseData);
-
-  res.status(200).json(successResponse);
+const logger = require('../../logger');
+module.exports = async (req, res) => {
+  logger.debug('req.query: ' + JSON.stringify(req.query));
+  let expand = req.query.expand === '1';
+  try {
+    const fragments = await Fragment.byUser(req.user, expand);
+    res.status(200).json(
+      createSuccessResponse({
+        fragments: fragments,
+      })
+    );
+  } catch (e) {
+    res.status(500).json(createErrorResponse(500, e.message));
+  }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -43,5 +43,3 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
-
-module.exports = router;
